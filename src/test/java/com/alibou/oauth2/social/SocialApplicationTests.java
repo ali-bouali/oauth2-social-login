29e9a237e31931cca693e60bcbce6a7fc2cf470d
alibouali <ali.bouali@mimacom.com>
2023-02-10 11:00:51 +0100

    init oath2-social-login project

---
+++ src/test/java/com/alibou/oauth2/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.oauth2.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
